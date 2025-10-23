36c89e63ff0ff13fcc18221eec575178e15cc6b4
Manjushree R G <manjushreerajendiran@gmail.com>
2025-10-23 23:26:09 +0530

    Add files via upload

---
+++ display.launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+
+def generate_launch_description():
+    urdf_file = os.path.join(
+        os.path.expanduser('~'),
+        'ros2_ws/src/my_robot_description/urdf/my_robot.urdf.xacro'
+    )
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': open(urdf_file).read()}]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen'
+        )
+    ])
